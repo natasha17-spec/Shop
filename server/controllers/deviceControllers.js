049526c21d2b835ba82870aecb28ff5072c6421f
Natalia Gerasimovich <17061990@inbox.ru>
2021-07-18 11:20:11 +0300

    add deviceControllers

---
+++ server/controllers/deviceControllers.js
@@ -0,0 +1,13 @@
+class DeviceControllers {
+    async create(req,res){
+
+    }
+    async getAll(req,res){
+
+    }
+    async getOne(req,res){
+
+    }
+
+}
+module.exports = new DeviceControllers()
